1020f51113e7280db85f6ffe28e5c26a5f1e7a45
soooheeee <tgb0626@naver.com>
2026-01-14 20:25:33 +0900

    chore: coderabbit 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>test</div>
+    </div>
+  );
 }
 
 export default App;
