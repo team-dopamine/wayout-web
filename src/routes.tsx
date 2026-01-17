19240b9c60334c9c82ec12a71b5a611f8df957f7
joohee kim <dev.joohee0916@gmail.com>
2026-01-17 16:14:10 +0900

    feat: counterexample 페이지 라우팅 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import MainPage from '@/pages/MainPage';
+import CounterExamplePage from './pages/CounterExamplePage';
 
 export const router = createBrowserRouter([
   {
     element: <App />,
-    children: [{ path: '/', element: <MainPage /> }],
+    children: [
+      { path: '/', element: <MainPage /> },
+      { path: '/counter-example', element: <CounterExamplePage /> },
+    ],
   },
 ]);
