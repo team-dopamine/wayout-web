e67d692b51ad897a40bca9d434d7e05c98cb9a96
joohee kim <dev.joohee0916@gmail.com>
2026-01-19 18:52:22 +0900

    feat: 정답 코드 및 테스트 케이스 제출 페이지 경로 라우팅

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import MainPage from '@/pages/MainPage';
 import CounterExamplePage from './pages/CounterExamplePage';
+import SolutionSubmitPage from './pages/SolutionSubmitPage';
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
     children: [
       { path: '/', element: <MainPage /> },
       { path: '/counter-example', element: <CounterExamplePage /> },
+      { path: '/solution-submit', element: <SolutionSubmitPage /> },
     ],
   },
 ]);
