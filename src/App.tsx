a09ce12b064bced61a6cc6d242eeb1eeed0abd2f
joohee kim <dev.joohee0916@gmail.com>
2026-01-19 18:55:02 +0900

    refactor: 헤더, 푸터를 공통 레이아웃으로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Header from '@/components/layout/Header';
+import FooterLinks from '@/components/layout/FooterLinks';
 
 function App() {
   return (
-    <div className="App">
-      <Outlet />
+    <div className="flex min-h-screen flex-col">
+      {/** 헤더 */}
+      <Header
+        onLogoClick={() => console.log('logo')}
+        onAllSubmissions={() => console.log('all submissions')}
+        onAllProblems={() => console.log('all problems')}
+        onSubmitSolution={() => console.log('submit solution')}
+        onSignIn={() => console.log('sign in')}
+      />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      {/** 푸터 */}
+      <FooterLinks
+        onAbout={() => console.log('about')}
+        onHelp={() => console.log('help')}
+        onContact={() => console.log('contact')}
+      />
     </div>
   );
 }
