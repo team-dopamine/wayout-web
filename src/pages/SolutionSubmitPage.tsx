4e4eba4f96c50a45ef81dbd19afb1ec54bbeaef6
joohee kim <dev.joohee0916@gmail.com>
2026-01-22 01:15:54 +0900

    chore: todo 주석 추가

--- src/pages/SolutionSubmitPage.tsx
+++ src/pages/SolutionSubmitPage.tsx
@@ -14,6 +14,7 @@ export default function SolutionSubmitPage() {
     <div className="min-h-screen bg-slate-50 transition-colors dark:bg-slate-900">
       <main className="mx-auto w-full max-w-5xl space-y-8 px-4 py-10 pb-32 text-slate-900 dark:text-slate-100 sm:px-6 lg:px-8">
         <SolutionCodeCard value={value} onChange={setValue} />
+        {/** TODO: 커스텀 테스트 케이스 상태 연동 필요 */}
         <CustomTestCasesSection onChange={() => {}} />
       </main>
     </div>
