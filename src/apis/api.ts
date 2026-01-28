98c61cae7bd40e5956063000efe385ddca6884c2
juiuj <151600782+juiuj@users.noreply.github.com>
2026-01-28 14:06:50 +0900

    [Feature](WO-78): 인증 API 연동을 위한 Axios 인터셉터 및 토큰 관리 구조 구현 (#20)

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,5 +1,6 @@
 /** 프로젝트 전역에서 사용할 Axios 공통 인스턴스 */
 import axios from 'axios';
+import { setupAuthInterceptors } from './auth/setupAuthInterceptors';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -8,5 +9,8 @@ const api = axios.create({
   },
   withCredentials: true,
 });
+
+/** 인증 인터셉터 등록 */
+setupAuthInterceptors(api);
 
 export default api;
