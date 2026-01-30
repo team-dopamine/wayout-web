bb4d85e99d5581beb34aa6f42445a0af6e25607e
joohee kim <dev.joohee0916@gmail.com>
2026-01-31 00:36:15 +0900

    feat: 앱 부팅 시 accessToken 기반 로그인 상태 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import DarkModeToggle from './components/common/DarkModeToggle';
 import { startGoogleOAuth } from './apis/auth/googleOAuth';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import api from './apis/api';
+import { ensureAuthBootstrapped } from './apis/auth/setupAuthInterceptors';
+import { getAccessToken } from '@/apis/auth/tokenStore';
 
 function App() {
+  const [isAuthed, setIsAuthed] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await ensureAuthBootstrapped(api);
+      setIsAuthed(Boolean(getAccessToken()));
+    })();
+  }, []);
+
   return (
     <div className="flex min-h-screen flex-col">
-      {/** 헤더 */}
-      <Header onSignIn={startGoogleOAuth} />
+      <Header isAuthed={isAuthed} onSignIn={startGoogleOAuth} />
 
       <main className="flex-1">
         <Outlet />
       </main>
 
-      {/** 다크 모드 토글 */}
       <div>
         <DarkModeToggle />
       </div>
 
-      {/** 푸터 */}
       <Footer
         onAbout={() => console.log('about')}
         onHelp={() => console.log('help')}
