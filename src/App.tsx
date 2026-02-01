949e858b24ba84db56d90960554f27c324d085da
joohee kim <dev.joohee0916@gmail.com>
2026-02-02 02:43:36 +0900

    fix: 인증 상태에서 Header에 로그아웃 핸들러 누락 문제 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,25 @@ import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { startGoogleOAuth } from './apis/auth/googleOAuth';
 import { useAuthBootstrap } from '@/hooks/useAuthBootstrap';
+import { session } from './apis/auth/session';
 
 function App() {
   const { isAuthed, isAuthInitialized } = useAuthBootstrap();
 
+  // TODO: 추후 로그아웃 연동 시 수정
+  const handleLogout = () => {
+    session.clearAccessToken();
+    window.location.href = '/';
+  };
+
   return (
     <div className="flex min-h-screen flex-col">
-      {isAuthInitialized && <Header isAuthed={isAuthed} onSignIn={startGoogleOAuth} />}
+      {isAuthInitialized &&
+        (isAuthed ? (
+          <Header isAuthed={true} onLogout={handleLogout} />
+        ) : (
+          <Header isAuthed={false} onSignIn={startGoogleOAuth} />
+        ))}
 
       <main className="flex-1">
         <Outlet />
