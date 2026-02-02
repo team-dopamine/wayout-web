7ebf5769b2b65b3c8015a61f89fef056707686b9
joohee kim <dev.joohee0916@gmail.com>
2026-02-02 22:18:01 +0900

    style: pretendard 폰트 전역 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import 'pretendard/dist/web/static/pretendard.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <>
     <RouterProvider router={router} />
