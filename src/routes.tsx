b8e0431fe7e0ede070be3158a679332ae6075ea7
joohee kim <dev.joohee0916@gmail.com>
2026-03-11 15:11:06 +0900

    feat: 라우트 파라미터 정의

--- src/routes.tsx
+++ src/routes.tsx
@@ -23,8 +23,8 @@ export const router = createBrowserRouter([
       {
         element: <ProblemShellLayout />,
         children: [
-          { path: '/counter-example', element: <CounterExamplePage /> },
-          { path: '/solution-submit', element: <SolutionSubmitPage /> },
+          { path: '/counter-example/:problemId', element: <CounterExamplePage /> },
+          { path: '/solution-submit/:problemId', element: <SolutionSubmitPage /> },
 
           // TODO: 추후 API 연동 후 링크 변경
           { path: '/submissions/id', element: <ProblemSubmissionsPage /> },
