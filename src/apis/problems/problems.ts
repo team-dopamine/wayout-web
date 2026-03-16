953f8c6631baeef3dc18958607334462bb4ecf53
soooheeee <tgb0626@naver.com>
2026-03-17 03:29:48 +0900

    feat: 문제 검색 api 구현 및 type 정의

--- src/apis/problems/problems.ts
+++ src/apis/problems/problems.ts
@@ -1,7 +1,31 @@
 import api from '@/apis/api';
-import type { GetProblemResponse, Problem } from './problems.type';
+import type { GetProblemResponse, GetProblemSearchResponse, ProblemSearch } from './problems.type';
 
-export async function getProblem(): Promise<Problem[]> {
-  const res = await api.get<GetProblemResponse>('/problems');
-  return res.data.content;
+// 문제 목록 조회
+export async function getProblem(
+  page = 0,
+  size = 8,
+  sort = 'createdAt,DESC',
+): Promise<GetProblemResponse> {
+  const res = await api.get<GetProblemResponse>('/problems', {
+    params: {
+      page,
+      size,
+      sort,
+    },
+  });
+
+  return res.data;
+}
+
+// 문제 검색 조회
+export async function getProblemSearch(keyword: string, limit = 10): Promise<ProblemSearch[]> {
+  const res = await api.get<GetProblemSearchResponse>('/problems/search', {
+    params: {
+      keyword,
+      limit,
+    },
+  });
+
+  return res.data;
 }
--- src/apis/problems/problems.type.ts
+++ src/apis/problems/problems.type.ts
@@ -9,4 +9,19 @@ export interface Problem {
 
 export interface GetProblemResponse {
   content: Problem[];
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  first: boolean;
+  last: boolean;
 }
+
+export interface ProblemSearch {
+  problemId: number;
+  problemNo: number;
+  title: string;
+  platform: string;
+}
+
+export type GetProblemSearchResponse = ProblemSearch[];
