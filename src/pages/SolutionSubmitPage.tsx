60a5c8fdf44e43d6432ef4c3f75265d11bdba7d7
soooheeee <tgb0626@naver.com>
2026-03-22 19:02:46 +0900

    refactor: solutions API 파일 구조 통합 및 import 경로 수정

--- src/pages/SolutionSubmitPage.tsx
+++ src/pages/SolutionSubmitPage.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { SolutionCodeCard } from '@/components/solutionsubmit/SolutionCodeCard';
 import { CustomTestCasesSection } from '@/components/solutionsubmit';
 import { DEFAULT_CODE_BY_LANG } from '@/constants/counterexample';
-import { postSolutionApi } from '@/apis/solutions/postSolutionApi';
+import { postSolutionApi } from '@/apis/solutions/solutions';
 
 const LANGUAGE_MAP = {
   cpp: 'CPP',
