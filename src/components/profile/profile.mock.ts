f26e102c47c144811ce44ee8d88e9d3b9f0b8d15
soooheeee <tgb0626@naver.com>
2026-03-22 19:20:35 +0900

    Revert "chore: 기여하기 관련 mock 파일 제거"
    This reverts commit b67252a35ace146a28599b32117648dd9621be58.

---
+++ src/components/profile/profile.mock.ts
@@ -0,0 +1,25 @@
+import type { Contribution } from '@/components/profile/ContributionsSection';
+
+export const mockContributions: Contribution[] = [
+  {
+    codeId: '8375',
+    problemName: 'Traveling Salesman',
+    language: 'Python 3',
+    type: 'Correct Code',
+    submittedAt: '2025-12-21 21:17',
+  },
+  {
+    codeId: '7220',
+    problemName: 'Population Movement',
+    language: 'Java 8',
+    type: 'Correct Code',
+    submittedAt: '2025-08-19 17:29',
+  },
+  {
+    codeId: '7219',
+    problemName: 'Population Movement',
+    language: 'Java 8',
+    type: 'Incorrect Code',
+    submittedAt: '2025-08-19 17:28',
+  },
+];
